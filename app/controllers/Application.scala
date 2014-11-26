25054e1bb99db1d8ad31784d9e0547a5de6f4f32
Rich Dougherty <rich@rd.gen.nz>
2014-11-27 06:50:30 +1300

    Example of using PartSources

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,15 +12,15 @@ object Application extends Controller {
   def index = Action.async {
     import java.io.File
     import com.ning.http.client.{ Request => AHCRequest, RequestBuilder }
-    import com.ning.http.multipart.{ FilePart, StringPart }
+    import com.ning.http.multipart._
 
     val ahcRequest: AHCRequest = new RequestBuilder("POST")
       .setUrl("http://localhost:9000/consume")
       .addBodyPart(new StringPart("string1", "foo"))
       .addBodyPart(new StringPart("string2", "bar"))
       .addBodyPart(new FilePart("file1", new File("/tmp/x"), "text/plain", "utf-8"))
-      .addBodyPart(new FilePart("file2", new File("/tmp/y"), "text/plain", "utf-8"))
-      .addBodyPart(new FilePart("file3", new File("/tmp/z"), "text/plain", "utf-8"))
+      .addBodyPart(new FilePart("file2", new FilePartSource(new File("/tmp/y")), "text/plain", "utf-8"))
+      .addBodyPart(new FilePart("file3", new ByteArrayPartSource("z", Array[Byte]('z'.toByte)), "text/plain", "utf-8"))
       .build()
 
     executeAhcRequest(ahcRequest).map { wsResponse =>
